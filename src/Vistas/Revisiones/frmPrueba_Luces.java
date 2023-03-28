44883563de648b01e072d0c6cd4f0e62ffb090b9
LeChustin <jostinaldaz4@gmail.com>
2023-03-28 10:12:23 -0500

    Commit para pull

--- src/Vistas/Revisiones/frmPrueba_Luces.java
+++ src/Vistas/Revisiones/frmPrueba_Luces.java
@@ -144,8 +144,8 @@ public class frmPrueba_Luces extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 12, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 530, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
